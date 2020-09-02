11eb3df60d808b42224583315f5184c4de21611d
Yaron <asaf.yaron@Verint.com>
2020-09-02 11:32:24 +0300

    test users creation

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+
+
+def add(x, y):
+    """add two number togather"""
+    return x + y
+
+
+def subtract(x, y):
+    """subtract two number togather"""
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,12 @@
+# from django.test import TestCase
+# from app.calc import add, subtract
+
+
+# class CalcTests(TestCase):
+#     def test_add_numbers(self):
+#         """Test the function of add to add number together"""
+#         self.assertEqual(add(3, 8), 11)
+
+#     def test_subtract_number(self):
+#         """Test the function of subtract to subtract number together"""
+#         self.assertEqual(subtract(3, 8), 5)
